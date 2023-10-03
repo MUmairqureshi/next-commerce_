841aeac48ee42109f3a8f8a2d50ce86050fae6d9
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2023-10-03 12:01:16 -0700

    update

--- components/logos/index.tsx
+++ components/logos/index.tsx
@@ -2,45 +2,8 @@ import React from 'react';
 
 export function Logos() {
   return (
-    // <div className='w-full h-full mx-auto pb-12     border-slate-200 pt-6 py-8 item-center px-6  mt-6 border-t-2 border-b-2 '>
-
-    //         <div className="flex contianer  m-14 px-16   overflow-x-auto overflow-scroll  md:flex-row justify-between lg:w-65  w-full   space-x-4 text-xl">
-
-    //             <div className="text-center  rounded-md  flex ">
-    //                 <div className="text-center w-full ">
-
-    //                     <img src="https://kirrinfinch.com/cdn/shop/files/logo-bustle_180x.png?v=1618855415" alt="Logo 1" className="  w-22  h-12     mx-auto mb-2 text-black " />
-
-    //                 </div>
-
-    //             </div>
-
-    //             <div className="text-center    rounded-md flex  mt-4 md:mt-0 ">
-    //                 <div className="text-center w-full ">
-
-    //                     <img src="https://kirrinfinch.com/cdn/shop/files/logo-atlantic_180x.png?v=1618855731" alt="Logo 2" className="  w-22  h-12      mx-auto mb-2 text-black" />
-    //                 </div>
-
-    //             </div>
-
-    //             <div className="text-center    rounded-md flex ">
-    //                 <div className="text-center w-full ">
-    //                     <img src="https://kirrinfinch.com/cdn/shop/files/logo-them_180x.png?v=1618855751" alt="Logo 3" className="  w-22  h-12      mx-auto mb-2 text-black" />
-    //                 </div>
-    //             </div>
-
-    //             <div className="text-center    rounded-md flex mt-4 md:mt-0  ">
-    //                 <div className="text-center w-full ">
-
-    //                     <img src="https://kirrinfinch.com/cdn/shop/files/logo-vice_180x.png?v=1618855786" alt="Logo 4" className="  w-22  h-12      mx-auto mb-2 text-black" />
-    //                 </div>
-    //             </div>
-    //         </div>
-
-    //     </div>
-
     <div className="mx-auto mt-6 w-full items-center border-b-2 border-t-2 border-slate-200 py-8 pb-12 pt-6">
-      <div className="lg:w-65 container m-4 flex w-full justify-between space-x-4 overflow-scroll overflow-x-auto text-xl md:flex-row md:px-4">
+      <div className="lg:w-65 l container m-4 flex w-full justify-between space-x-4  text-xl md:flex-row md:px-4">
         <div className="flex rounded-md text-center">
           <div className="w-full">
             <img
@@ -82,31 +45,5 @@ export function Logos() {
         </div>
       </div>
     </div>
-
-    //         <div className='w-full h-full mx-auto pb-12 item-center px-6 border-t-2 border-b-2'>
-
-    //   <div className="overflow-x-auto overflow-scroll">
-    //     <div className="flex space-x-4 md:space-x-8">
-
-    //       <div className="w-26  h p-4 rounded-md text-center">
-    //         <img src="https://kirrinfinch.com/cdn/shop/files/logo-bustle_180x.png?v=1618855415" alt="Logo 1" className="w-full h-full text-black" />
-    //       </div>
-
-    //       <div className="w-26  h p-4 rounded-md text-center">
-    //         <img src="https://kirrinfinch.com/cdn/shop/files/logo-atlantic_180x.png?v=1618855731" alt="Logo 2" className="w-full h-full text-black" />
-    //       </div>
-
-    //       <div className="w-26  h p-4 rounded-md text-center">
-    //         <img src="https://kirrinfinch.com/cdn/shop/files/logo-them_180x.png?v=1618855751" alt="Logo 3" className="w-full h-full text-black" />
-    //       </div>
-
-    //       <div className="w-26  h p-4 rounded-md text-center">
-    //         <img src="https://kirrinfinch.com/cdn/shop/files/logo-vice_180x.png?v=1618855786" alt="Logo 4" className="w-full h-full text-black" />
-    //       </div>
-
-    //     </div>
-    //   </div>
-
-    // </div>
   );
 }
